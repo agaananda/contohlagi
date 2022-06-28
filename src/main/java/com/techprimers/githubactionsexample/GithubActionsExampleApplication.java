e28b0f7336c95a71c26fc72cb982dd31bf16b1cd
agaananda <saidanandaa@gmail.com>
2022-06-28 13:24:22 +0700

    Update GithubActionsExampleApplication.java

--- src/main/java/com/techprimers/githubactionsexample/GithubActionsExampleApplication.java
+++ src/main/java/com/techprimers/githubactionsexample/GithubActionsExampleApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GithubActionsExampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GithubActionsExampleApplication.class, args);
+		SpringApplicationn.run(GithubActionsExampleApplication.class, args);
 	}
 
 }
