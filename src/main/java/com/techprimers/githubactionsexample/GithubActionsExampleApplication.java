156f7ae93be267f871e3e6a96ff1b00fb3653bd6
agaananda <saidanandaa@gmail.com>
2022-06-29 13:06:30 +0700

    Update GithubActionsExampleApplication.java

--- src/main/java/com/techprimers/githubactionsexample/GithubActionsExampleApplication.java
+++ src/main/java/com/techprimers/githubactionsexample/GithubActionsExampleApplication.java
@@ -8,13 +8,15 @@ public class GithubActionsExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GithubActionsExampleApplication.class, args);
-		SpringApplication.run(GithubActionsExampleApplication.class, args)
-		SpringApplication.run(GithubActionsExampleApplication.class, args)
-		SpringApplication.run(GithubActionsExampleApplication.class, args)
+		
+	}
+	public static void main(String[] args) {
 		SpringApplication.run(GithubActionsExampleApplication.class, args);
+		
+	}
+	public static void main(String[] args) {
 		SpringApplication.run(GithubActionsExampleApplication.class, args);
-		SpringApplication.run(GithubActionsExampleApplication.class, args);
-		SpringApplication.run(GithubActionsExampleApplication.class, args);
+		
 	}
 }
 
