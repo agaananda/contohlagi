560cbf9c67384d19832719c981cbe309a6a4b2ef
agaananda <saidanandaa@gmail.com>
2022-06-29 12:36:10 +0700

    commit

--- src/main/java/com/techprimers/githubactionsexample/GithubActionsExampleApplication.java
+++ src/main/java/com/techprimers/githubactionsexample/GithubActionsExampleApplication.java
@@ -6,24 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GithubActionsExampleApplication {
 
-<<<<<<< Updated upstream
-	public static void main(String[] args) {
-		SpringApplication.run(GithubActionsExampleApplication.class, args);
-=======
 	public static void main(String[] contoh) {
 		SpringApplication.run(GithubActionsExampleApplication.class, contoh);
-		SpringApplication.run(GithubActionsExampleApplication.class, contoh);
-
-		SpringApplication.run(GithubActionsExampleApplication.class, contoh);
-
-		SpringApplication.run(GithubActionsExampleApplication.class, contoh);		
 		
-		SpringApplication.run(GithubActionsExampleApplication.class, contoh);
 
 
-
-
->>>>>>> Stashed changes
 	}
 
 }
